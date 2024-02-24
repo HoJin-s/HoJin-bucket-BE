6c53be748753f1dff51d34724e11536666533680
USER\HOME <leekh9997@naver.com>
2024-02-25 00:51:10 +0900

    Feat: 회원가입 기능 추가 & 해싱과 이메일 형식 검증 install

---
+++ domain/user/user_schema.py
@@ -0,0 +1,24 @@
+import datetime
+
+from pydantic import BaseModel, field_validator, EmailStr
+from pydantic_core.core_schema import FieldValidationInfo
+
+
+# 회원가입
+class UserCreate(BaseModel):
+    password: str
+    password_check: str
+    username: str
+    email: EmailStr
+
+    @field_validator("password", "password_check", "username", "email")
+    def not_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError("빈 값은 허용되지 않습니다.")
+        return v
+
+    @field_validator("password_check")
+    def passwords_match(cls, v, info: FieldValidationInfo):
+        if "password" in info.data and v != info.data["password"]:
+            raise ValueError("비밀번호가 일치하지 않습니다")
+        return v
