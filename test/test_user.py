7cfcfa4df8e0c7d3a2d8d508a2cc626830ba9781
USER\HOME <leekh9997@naver.com>
2024-02-29 14:00:20 +0900

    Feat: 회원가입 성공 및 실패 테스트코드 작성

--- test/test_user.py
+++ test/test_user.py
@@ -46,3 +46,140 @@ async def test_read_user_list_with_fifty_users(
     assert response.json()["data"][0]["email"] == fifty_test_users[0].email
     assert response.json()["data"][49]["email"] == fifty_test_users[49].email
 
+
+# 회원가입 POST 유저정보 테스트
+@pytest.mark.asyncio
+async def test_user_register_success_adds_to_db(test_session: AsyncSession) -> None:
+    response = client.post(
+        url="/api/user/create",
+        json={
+            "email": "leekh9997@naver.com",
+            "username": "이기호",
+            "password": "password",
+            "password_check": "password",
+        },
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+
+    results = await test_session.execute(select(User))
+    users = results.scalars().all()
+
+    assert len(users) == 1
+    assert users[0].email == "leekh9997@naver.com"
+    assert users[0].username == "이기호"
+
+
+# 회원가입 POST 시, 빈값 입력
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "invalid_data",
+    [
+        {
+            "email": "",  # email 빈값
+            "username": "이기호",
+            "password": "password",
+            "password_check": "password",
+        },
+        {
+            "email": "leekh9997@naver.com",
+            "username": "",  # username 빈값
+            "password": "password",
+            "password_check": "password",
+        },
+        {
+            "email": "leekh9997@naver.com",
+            "username": "이기호",
+            "password": "",  # password 빈값
+            "password_check": "password",
+        },
+        {
+            "email": "leekh9997@naver.com",
+            "username": "이기호",
+            "password": "password",
+            "password_check": "",  # password_check 빈값
+        },
+    ],
+)
+async def test_register_user_with_empty_fields(invalid_data: dict[str, str]) -> None:
+    response = client.post(
+        url="/api/user/create",
+        json=invalid_data,
+    )
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+# 회원가입 POST 시, 패스워드 불일치
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "invalid_data",
+    [
+        {
+            "email": "leekh9997@naver.com",
+            "username": "이기호",
+            "password": "password",
+            "password_check": "different_password",  # password_check 불일치
+        },
+    ],
+)
+async def test_register_user_with_passwords_mismatch(
+    invalid_data: dict[str, str]
+) -> None:
+    response = client.post(
+        url="/api/user/create",
+        json=invalid_data,
+    )
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+# 회원가입 POST 시, email 중복
+@pytest.mark.asyncio
+async def test_register_user_with_duplicated_email(one_test_user: User) -> None:
+    response = client.post(
+        url="/api/user/create",
+        json={
+            "email": one_test_user.email,
+            "username": "이기호",
+            "password": "password",
+            "password_check": "password",
+        },
+    )
+
+    assert response.status_code == status.HTTP_409_CONFLICT
+
+
+# 회원가입 POST 시, username 중복
+@pytest.mark.asyncio
+async def test_register_user_with_duplicated_username(one_test_user: User) -> None:
+    response = client.post(
+        url="/api/user/create",
+        json={
+            "email": "leekh9997@naver.com",
+            "username": one_test_user.username,
+            "password": "password",
+            "password_check": "password",
+        },
+    )
+
+    assert response.status_code == status.HTTP_409_CONFLICT
+
+
+# 회원가입 POST 시, 비밀번호가 암호화되어 저장되는지 검증
+@pytest.mark.asyncio
+async def test_password_is_hashed(test_session: AsyncSession) -> None:
+    response = client.post(
+        url="/api/user/create",
+        json={
+            "email": "leekh9997@naver.com",
+            "username": "이기호",
+            "password": "password",
+            "password_check": "password",
+        },
+    )
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+    results = await test_session.execute(select(User))
+    users = results.scalars().all()
+    assert users[0].password != "password"
