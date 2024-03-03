51e39c4779d2b3a2890c9830ee84fe58a93c7372
USER\HOME <leekh9997@naver.com>
2024-03-03 23:00:10 +0900

    Fix: load_dotenv(override=True)로 변경

--- domain/user/user_router.py
+++ domain/user/user_router.py
@@ -16,7 +16,7 @@ import os
 
 from models import User
 
-load_dotenv()
+load_dotenv(override=True)
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
--- test/test_user.py
+++ test/test_user.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 import os
 
 # 로그인 테스트 시, 사용
-load_dotenv()
+load_dotenv(override=True)
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
