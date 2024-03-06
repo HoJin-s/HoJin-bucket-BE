8451459a06f267b43960af69de83c17bdb615f6a
USER\HOME <leekh9997@naver.com>
2024-03-07 01:14:25 +0900

    Feat: 버킷리스트 수정 테스트코드 작성

--- test/test_bucketlist.py
+++ test/test_bucketlist.py
@@ -135,7 +135,7 @@ async def test_read_bucketlist_detail_failure(one_test_bucketlist: BucketList) -
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
-# bucketlist 생성 POST
+# bucketlist 생성 POST 성공
 @pytest.mark.asyncio
 async def test_create_bucketlist(test_login_and_get_token) -> None:
 
@@ -248,3 +248,50 @@ async def test_create_bucketlist_with_wrong_calender(test_login_and_get_token) -
         response.json()["detail"][0]["ctx"]["error"]
         == "day value is outside expected range"
     )
+
+
+# bucketlist 수정 PUT 성공
+@pytest.mark.asyncio
+async def test_update_bucketlist(
+    one_test_bucketlist: BucketList,
+    test_login_and_get_token,
+) -> None:
+    """
+    bucketlist 수정 기능은 {bucketlist_id}가 추가된 것 말고는 bucketlist 생성과 에러가 같습니다.
+    (title 미입력/빈값, category/calender 부적절 등)
+    """
+    response = client.put(
+        url="/api/bucketlist/update",
+        headers={"Authorization": f"Bearer {test_login_and_get_token}"},
+        json={
+            "bucketlist_id": one_test_bucketlist.id,
+            "title": "updated_one_test_bucketlist_title",
+            "content": "updated_test_bucketlist_create_content",
+            "category": "액티비티",
+            "calender": "2024-03-06",
+        },
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["status"] == "200"
+    assert response.json()["success"] == "버킷리스트 수정완료"
+
+
+# bucketlist 수정 PUT 실패 (잘못된 bucketlist_id)
+@pytest.mark.asyncio
+async def test_update_bucketlist_wrong_bucketlist_id(
+    one_test_bucketlist: BucketList,
+    test_login_and_get_token,
+) -> None:
+    response = client.put(
+        url="/api/bucketlist/update",
+        headers={"Authorization": f"Bearer {test_login_and_get_token}"},
+        json={
+            "bucketlist_id": one_test_bucketlist.id + 1,
+            "title": "updated_one_test_bucketlist_title",
+            "content": "updated_test_bucketlist_create_content",
+            "category": "액티비티",
+            "calender": "2024-03-06",
+        },
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json()["detail"] == "해당 게시글을 찾을 수 없습니다."
